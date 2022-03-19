39318865f0fc266a0d1aae1c0d947fda1998ff5e
Jakhongir <77021074+jakhongir12@users.noreply.github.com>
2022-03-20 03:23:46 +0500

    my project

---
+++ index.js
@@ -0,0 +1,17 @@
+// 4 * 3.14 * 3390 * 3390;
+// const pi = 3.14;
+// 4 * pi * 3390 * 3390; // hello
+// const surface = 4 * pi * 3390 * 3390;
+// // console.log(surface);
+
+// const surfaceAreaCalculator = (radius) =>{
+//     return 4 * 3.14 * radius * radius;
+// }
+// const surfaceOfMars = surfaceAreaCalculator(3390);
+// // console.log(surfaceOfMars);
+// const percentageCalculator = (number, total) =>{
+//     return number * 100 / total;
+// }
+// // console.log(percentageCalculator(8,31));
+
+// 512 === 988;
